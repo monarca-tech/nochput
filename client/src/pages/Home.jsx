bb22101a8737adbc1e4d9ded56ead7ae6c91de01
juan manuel <monarcawork8@gmail.com>
2025-02-10 15:40:42 -0400

    routar para renderisar mejor

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 function Home() {
   return (
-    <div>welcame to my website</div>
+    <div>
+      <h1>welcame to my site web , the page web is an list task and un site for create gold </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam rerum soluta quos aperiam sapiente debitis sed repudiandae quisquam blanditiis, praesentium ullam possimus vitae, saepe quia. Ipsam, possimus explicabo! Culpa consequatur eaque laborum et nesciunt nostrum accusantium rerum, distantium rerum, distantium rerum, distantium rerum, distantium rerum, distantium rerum, distantium rerum, distantium rerum, distantium rerum, distantium rerum, distantium rerum, distinctio minus ea!</p>
+    </div>
   )
 }
 
